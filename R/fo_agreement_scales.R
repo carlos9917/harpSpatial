dfead4c56ce1069c51521abfbe20a5b20bf6c186
Carlos Peralta <carlos9917@gmail.com>
2025-08-05 13:07:40 +0000

    modified fo_agreement_scale to return matrix or not

--- R/fo_agreement_scales.R
+++ R/fo_agreement_scales.R
@@ -148,7 +148,7 @@ agreement_scale_map <- function(f1, f2, alpha = 0.5, S_lim = 80L, verbose = TRUE
 #' @return A tibble with the agreement scale map (SA_fo) and its summary statistics.
 #'   The tibble has one row, with the SA_fo matrix in a list-column.
 #' @export
-fo_agreement_scales <- function(fcfield, obfield, alpha = 0.5, S_lim = 80L, verbose=TRUE, ...) {
+fo_agreement_scales <- function(fcfield, obfield, alpha = 0.5, S_lim = 80L, verbose=TRUE, return_full_matrix = FALSE, ...) {
 
   obs_dims <- dim(obfield)
   obs_values <- as.numeric(obfield)
@@ -180,11 +180,21 @@ fo_agreement_scales <- function(fcfield, obfield, alpha = 0.5, S_lim = 80L, verb
     sd_agreescale   = sd(SA_fo, na.rm = TRUE)
   )
 
-  tibble::tibble(
-    SA_fo               = list(SA_fo),
-    mean_agreescale     = summary_stats$mean_agreescale,
-    min_agreescale      = summary_stats$min_agreescale,
-    max_agreescale      = summary_stats$max_agreescale,
-    sd_agreescale       = summary_stats$sd_agreescale
-  )
+  if (return_full_matrix) {
+    tibble::tibble(
+      SA_fo               = list(SA_fo),
+      mean_agreescale     = summary_stats$mean_agreescale,
+      min_agreescale      = summary_stats$min_agreescale,
+      max_agreescale      = summary_stats$max_agreescale,
+      sd_agreescale       = summary_stats$sd_agreescale
+    )
+  } else {
+    tibble::tibble(
+      SA_fo               = summary_stats$mean_agreescale,
+      mean_agreescale     = summary_stats$mean_agreescale,
+      min_agreescale      = summary_stats$min_agreescale,
+      max_agreescale      = summary_stats$max_agreescale,
+      sd_agreescale       = summary_stats$sd_agreescale
+    )
+  }
 }
